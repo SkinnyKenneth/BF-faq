3113759c50e30cff33b6ed8e8de71aed84d9175b
SkinnyKenneth <134853391+SkinnyKenneth@users.noreply.github.com>
2023-10-05 16:43:09 +0200

    initial push after building header and search bar

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// show dropdown suggestions for input
+const optionsList = document.querySelector(".options");
+const searchBar = document.querySelectorAll(".input-wrapper > *")
+
+searchBar.forEach(el => {
+    document.addEventListener("click", (e) => {
+        e.target == el 
+            ? optionsList.classList.add("open")
+            : optionsList.classList.remove("open");
+    })
+
+})
+
+// autofill input field by clicking from dropdown
+const optionsListEls = optionsList.querySelectorAll("li");
+const searchInput = document.getElementById("searchQuery");
+
+optionsListEls.forEach(option => {
+    option.addEventListener("click", (e) => {
+        searchQuery.value = e.target.innerText;
+    })
+})
